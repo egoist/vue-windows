0b3851222a4c192711d28cfe831e75a8dc5b97a4
EGOIST <0x142857@gmail.com>
2016-10-05 15:29:35 +0800

    update example

--- example/App.vue
+++ example/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
     <div class="example">
-      <editor-window title="my window" :width="width" :height="height">
-        hola!
+      <editor-window title="How To" :width="width" :height="height">
+        <pre><code v-html="code"></code></pre>
       </editor-window>
-      <browser-window url="http://example.com" :width="width" :height="height">
-        your webpage!
+      <browser-window url="https://github.com/egoist/vue-windows" :width="width" :height="height">
+        <div class="content">
+          <a class="star" href="https://github.com/egoist/vue-windows">Start me on GitHub!</a>
+        </div>
       </browser-window>
     </div>
     <div class="control">
       <div class="range">
-        Width: <input type="range" min="460" max="600" v-model="width">
+        Width: <input type="range" min="460" max="600" v-model.number="width">
         <span v-show="width">{{ width }} px</span>
       </div>
       <div class="range">
-        Height: <input type="range" min="100" max="600" v-model="height">
+        Height: <input type="range" min="100" max="600" v-model.number="height">
         <span v-show="height">{{ height}} px</span>
       </div>
     </div>
@@ -22,13 +24,28 @@
 </template>
 
 <script>
+  import hljs from 'highlight.js/lib/highlight'
   import {EditorWindow, BrowserWindow} from '../src'
+
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
 
   export default {
     data() {
       return {
         width: 500,
-        height: 340
+        height: 340,
+        code: hljs.highlight('javascript', `import {EditorWindow} from 'vue-windows'
+
+new Vue({
+  el: '#app',
+  render(h) {
+    return h(EditorWindow, {props: {title: 'hola!'}})
+  }
+})
+
+// set how to use in single file component
+// https://github.com/egoist/vue-windows#usage
+`).value
       }
     },
     components: {
@@ -39,6 +56,9 @@
 </script>
 
 <style>
+  * {
+    box-sizing: border-box;
+  }
   body {
     font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
   }
@@ -58,4 +78,21 @@
     justify-content: center;
     align-items: center;
   }
+
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  a.star {
+    color: #4fc08d;
+    text-decoration: none;
+    border: 1px solid #4fc08d;
+    border-radius: 33px;
+    padding: 10px 20px;
+  }
 </style>
+
+<style src="highlight.js/styles/github"></style>
